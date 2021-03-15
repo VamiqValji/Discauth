acea8e4412c608af2aa928b51778a4666e2cca2c
Vamiq <vamiq.valji@gmail.com>
2021-03-15 19:50:25 -0400

    setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from "./pages/Test";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
---
+++ client/src/pages/Test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface TestProps {
+
+}
+
+const Test: React.FC<TestProps> = ({}) => {
+        return (
+            <>
+                <h2>test page</h2>
+            </>
+        );
+}
+
+export default Test;
