69782b87bc96abc0224b0052ca8079f8d1b0556d
Vamiq <vamiq.valji@gmail.com>
2021-04-02 19:06:49 -0400

    added interfaces

---
+++ bot/src/utils/interface/ownersInterface.ts
@@ -0,0 +1,28 @@
+import { user } from "./interface";
+
+interface ownersDocumentServers {
+    serverId: string,
+    serverName: string,
+    icon: string,
+    verificationCodes: string,
+    ownerVerified: boolean,
+    users: user
+}
+
+interface ownersDocumentVerificationCodes {
+    googleId: string,
+    serverName: string,
+    code: string,
+    discordId: string,
+    discordName: string,
+    avatar: string,
+}
+
+export default interface ownersDocument {
+    servers: ownersDocumentServers,
+    verificationCodes: ownersDocumentVerificationCodes,
+    discordId: string,
+    discordName: string,
+    email: string,
+    googleId: string,
+}
