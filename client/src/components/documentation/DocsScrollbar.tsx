3c9ccc34cee57b038ce4b542aa6651f91e81bdf0
Vamiq <vamiq.valji@gmail.com>
2021-04-03 16:08:54 -0400

    basic documentation layout

--- client/src/components/documentation/DocsScrollbar.tsx
+++ client/src/components/documentation/DocsScrollbar.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import "./Documentation.scss";
 
 interface DocsScrollbarProps {}
 
 const DocsScrollbar: React.FC<DocsScrollbarProps> = ({}) => {
     return (
     <>
-        scrollbar
+        <div className="documentationContainer">
+            <ul className="documentationScrollbar customScrollbarDark">
+                <li className="header">Introduction</li>
+                <li>Section 1</li>
+                <li>Section 2</li>
+                <li>Section 3</li>
+                <li>Section 4</li>
+                <li>Section 5</li>
+            </ul>
+
+            <div className="documentationWriting">
+                <h2>Introduction</h2>
+                <p></p>
+                <h2>Getting Started</h2>
+            </div>
+        </div>
     </>);
 }
 
