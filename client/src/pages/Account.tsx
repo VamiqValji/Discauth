89fd1550be974a3c881500f2223d8dce6121754b
Vamiq <vamiq.valji@gmail.com>
2021-04-04 22:59:48 -0400

    this was supposed to go with previous commit

---
+++ client/src/pages/Account.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AccountProps {}
+
+const Account: React.FC<AccountProps> = ({}) => {
+    return (
+    <>
+        <br/>
+        <h1>Account</h1>
+    </>);
+}
+
+export default Account;
