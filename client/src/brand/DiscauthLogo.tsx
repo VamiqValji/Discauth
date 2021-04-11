5acca4f018bdf4b41b1ab009ec96602baa60ed97
Vamiq <vamiq.valji@gmail.com>
2021-04-11 03:58:26 -0400

    added logo svg as react component

---
+++ client/src/brand/DiscauthLogo.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DiscauthLogo: React.FC = (/*{}*/) => {
+    return (
+    <>
+        <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Logo">
+            <g id="Entire Logo">
+            <rect id="45degRect" x="256" width="362.039" height="362.039" transform="rotate(45 256 0)" fill="white"/>
+            <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="131" y="155" width="250" height="202">
+            <g id="checkmark">
+            <rect id="top" x="378.039" y="196.52" width="222.764" height="55.0616" transform="rotate(135 378.039 196.52)" fill="#C4C4C4" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            <rect id="bottom" x="173.033" y="228.71" width="121.998" height="55.0616" transform="rotate(45 173.033 228.71)" fill="#C4C4C4" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            <rect id="blockInsideStroke" x="339.096" y="159.729" width="52.0275" height="219.736" transform="rotate(45 339.096 159.729)" fill="#C4C4C4"/>
+            </g>
+            </mask>
+            <g mask="url(#mask0)">
+            <rect id="gradient" x="131.523" y="155.009" width="252.477" height="202.972" fill="url(#paint0_linear)"/>
+            </g>
+            </g>
+            </g>
+            <defs>
+            <linearGradient id="paint0_linear" x1="257.761" y1="155.009" x2="257.761" y2="357.981" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#8E44AD"/>
+            <stop offset="1" stop-color="#9B59B6"/>
+            </linearGradient>
+            </defs>
+        </svg>
+    </>);
+}
+
+export default DiscauthLogo;
--- client/src/components/NavBar/Nav.tsx
+++ client/src/components/NavBar/Nav.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Login from '../../pages/Login';
 import "./Nav.scss";
-import DiscauthLogo from "../../brand/DiscauthLogo.svg";
+// import DiscauthLogo from "../../brand/DiscauthLogo.svg";
+import DiscauthLogo from "../../brand/DiscauthLogo";
 
 interface NavProps {}
 
@@ -13,29 +14,8 @@ const Nav: React.FC<NavProps> = (/*{}*/) => {
             <nav>
                 <ul>
                     <span className="leftNav">
-                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g id="Logo">
-<g id="Entire Logo">
-<rect id="45degRect" x="256" width="362.039" height="362.039" transform="rotate(45 256 0)" fill="white"/>
-<mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="131" y="155" width="250" height="202">
-<g id="checkmark">
-<rect id="top" x="378.039" y="196.52" width="222.764" height="55.0616" transform="rotate(135 378.039 196.52)" fill="#C4C4C4" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-<rect id="bottom" x="173.033" y="228.71" width="121.998" height="55.0616" transform="rotate(45 173.033 228.71)" fill="#C4C4C4" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-<rect id="blockInsideStroke" x="339.096" y="159.729" width="52.0275" height="219.736" transform="rotate(45 339.096 159.729)" fill="#C4C4C4"/>
-</g>
-</mask>
-<g mask="url(#mask0)">
-<rect id="gradient" x="131.523" y="155.009" width="252.477" height="202.972" fill="url(#paint0_linear)"/>
-</g>
-</g>
-</g>
-<defs>
-<linearGradient id="paint0_linear" x1="257.761" y1="155.009" x2="257.761" y2="357.981" gradientUnits="userSpaceOnUse">
-<stop stop-color="#8E44AD"/>
-<stop offset="1" stop-color="#9B59B6"/>
-</linearGradient>
-</defs>
-</svg>
+                        {/* <img src={DiscauthLogo} alt="Discauth Logo"/> */}
+                        <DiscauthLogo />
                         <h2>Discauth</h2>
                     </span>
                     <span className="middleNav">
