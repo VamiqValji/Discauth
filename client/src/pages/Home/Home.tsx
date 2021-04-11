4bbabf6a9d728b63973c326f9e004adcc5f88c90
Vamiq <vamiq.valji@gmail.com>
2021-04-11 19:57:18 -0400

    basic landing page layout built

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -8,7 +8,20 @@ interface HomeProps {
 const Home: React.FC<HomeProps> = (/*{}*/) => {
     return (
         <>
-            <h2>Home</h2>
+            <div className="landingContainer">
+                <div className="landing">
+                    <div className="content">
+                        <div className="left">
+                            <h1>Discord server verification made easy</h1>
+                            <h2>The ultimate server member verification system for Discord. Everything you need, connected with a bot and web application.</h2>
+                            <button>Join Now</button>
+                        </div>
+                        <div className="right">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
