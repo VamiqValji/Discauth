8ba2bfda975e9394333e41b7ecbdfb0091201b3f
Vamiq <vamiq.valji@gmail.com>
2021-04-12 19:25:02 -0400

    features text mostly done

--- client/src/pages/Home/HomeComponents/Features.tsx
+++ client/src/pages/Home/HomeComponents/Features.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 interface FeaturesProps {}
 
 interface feature {
-    showcase: any,
-    header: string,
-    paragraph: string,
+    showcase: JSX.Element,
+    header: JSX.Element,
+    paragraph: JSX.Element,
 }
 
 const Features: React.FC<FeaturesProps> = () => {
 
     const features:feature[] = [
         {
-            showcase: "Picture Here",
-            header: "Easy",
-            paragraph: "Verification is simple and easy."
+            showcase: <>Picture Here</>,
+            header: <>Easy To Start</>,
+            paragraph: <>Get started as a server owner by simply logging into the website and following the <Link to="/documentation">documentation</Link>.</>
         },
         {
-            showcase: "Picture Here",
-            header: "Easy",
-            paragraph: "Verification is simple and easy."
+            showcase:<>Picture Here</>,
+            header: <>Free</>,
+            paragraph: <>Discauth's starts with a free tier, so that you may upgrade only if your server has greater needs.</>
         },
         {
-            showcase: "Picture Here",
-            header: "Easy",
-            paragraph: "Verification is simple and easy."
+            showcase:<>Picture Here</>,
+            header: <>Speed</>,
+            paragraph: <>Server owners must only go through a few quick steps to register their server. The same applies to server members, thought with even less steps! More information <Link to="/documentation">here</Link>.</>
+        },
+        {
+            showcase:<>Picture Here</>,
+            header: <>Trustful Information Based Verification</>,
+            paragraph: <>By getting your server members to verify with their email, you can trust that they are serious about your server. This is especially helpful for exclusive chat channels or tournament-based servers.</>
+        },
+        {
+            showcase:<>Picture Here</>,
+            header: <>Displayed Data</>,
+            paragraph: <>You can easily view all of your server members and their verified data, for each server you have registered using Discauth.</>
+        },
+        {
+            showcase:<>Picture Here</>,
+            header: <>Other</>,
+            paragraph: <>Other</>
         },
     ];
 
--- client/src/pages/Home/HomeComponents/Landing.tsx
+++ client/src/pages/Home/HomeComponents/Landing.tsx
@@ -10,7 +10,7 @@ const Landing: React.FC<LandingProps> = () => {
                 <div className="content">
                     <div className="left">
                         <h1>Discord server member verification made easy</h1>
-                        <h2>The ultimate server member verification system for Discord. Everything you need, connected with a bot and web application.</h2>
+                        <h2>The ultimate server member verification system for Discord. Everything a server owner needs, connected with a bot and web application.</h2>
                         <button>Join Now</button>
                     </div>
                     <div className="right">
