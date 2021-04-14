39d28247a8d8649851fb72a04b46585c64669719
Vamiq <vamiq.valji@gmail.com>
2021-04-14 18:13:25 -0400

    modal renders on sign up / log out

--- client/src/components/LoginModal/LoginModal.tsx
+++ client/src/components/LoginModal/LoginModal.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import Login from '../../pages/Login';
 
-interface LoginModalProps {}
+interface LoginModalProps {
+    active: boolean,
+    toggleParentFunction: () => void,
+}
 
-const LoginModal: React.FC<LoginModalProps> = ({}) => {
-    return (
-    <>
-        
-    </>);
+const LoginModal: React.FC<LoginModalProps> = ({ active, toggleParentFunction }) => {
+    if (active) {
+        return (
+            <>
+                <div className="popBG">
+                    <div className="popContainer">
+                        <div className="pop">
+                            <h1 className="lightSuccess">You are logged in!</h1>
+                            <h1 className="whiteSmoke">{"message"}</h1>
+                            <div className="flexCenter">
+                                <Login />
+                                <button onClick={() => {
+                                    toggleParentFunction();
+                                }}>X</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>);
+    }
+    return <></>;
 }
 
 export default LoginModal;
--- client/src/components/NavBar/RightNav.tsx
+++ client/src/components/NavBar/RightNav.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../../pages/Login';
+import { useSelector } from "react-redux";
+import { loggedInformation } from '../../ts/interface';
+import LoginModal from '../LoginModal/LoginModal';
 
 interface RightNavProps {}
 
 const RightNav: React.FC<RightNavProps> = () => {
+    const [modalIsOn, setModalIsOn] = useState<boolean>(false);
+    const { loggedIn }:loggedInformation = useSelector((state:any) => state.loggedInfo);
+
+    const toggleModal = () => {
+        setModalIsOn(!modalIsOn);
+    };
+
     return (
         <li style={{padding: 0}}>
-            <button className="homeButton" style={{width: "100%"}}>Login</button>
+            <button className="homeButton" style={{width: "100%"}} onClick={() =>{
+                toggleModal();
+            }}>{loggedIn ? "Log Out" : "Login / Sign Up"}</button>
+            <LoginModal active={modalIsOn} toggleParentFunction={toggleModal} />
             <span style={{display: "none"}}>
                 <Login />
             </span>
