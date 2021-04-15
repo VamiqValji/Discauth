10259a343f981234e2bc9abcf9ced5f45fa4de4b
Vamiq <vamiq.valji@gmail.com>
2021-04-14 23:36:21 -0400

    join now buttons now toggle loginModal

--- client/src/components/JoinNowButton.tsx
+++ client/src/components/JoinNowButton.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { loginModalState } from '../ts/interface';
+import { activateLoginModal, deactivateLoginModal } from "../actions/loginModalAction";
+import { useSelector, useDispatch } from "react-redux";
 
 interface JoinNowButtonProps {
     extraClasses?: string,
 }
 
 const JoinNowButton: React.FC<JoinNowButtonProps> = ({ extraClasses }) => {
+    const dispatch = useDispatch();
+    const { active }:loginModalState = useSelector((state:any) => state.loginModalState);
+
+    const toggleModal = () => {
+        if (active) {
+            dispatch(deactivateLoginModal());
+        } else {
+            dispatch(activateLoginModal());
+        }
+    };
+
     return (
-        <button className={`homeButton ${extraClasses}`}>Join Now</button>
+        <button className={`homeButton ${extraClasses}`} onClick={toggleModal}>
+            Join Now
+        </button>
     );
 }
 
