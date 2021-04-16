98b7da81195f9a7734aa9f634594e16fd07c1594
Vamiq <vamiq.valji@gmail.com>
2021-04-16 18:56:24 -0400

    visual selector highlighting of which tab is active added

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,13 +4,15 @@ import MyServers from "../components/MyServers";
 import Setup from "../components/Setup";
 import Modal from "../components/Modal";
 
+import "../components/componentStyles/Dashboard.scss";
+
 interface DashboardProps {
 
 }
 
 const Dashboard: React.FC<DashboardProps> = (/*{}*/) => {
 
-    const [panel, setPanel] = useState<string>("");
+    const [panel, setPanel] = useState<string>("Setup");
 
     const updatePanel = (e: React.MouseEvent) => {
         setPanel(e.currentTarget.innerHTML);
@@ -19,22 +21,24 @@ const Dashboard: React.FC<DashboardProps> = (/*{}*/) => {
     console.log(panel);
 
     return (
-        <div className="appContainer">  
-            <Modal message={"Please Login."} />
-            <br/>
-            <h1>Owner Dashboard</h1>
-            <br/>
-            <button className="noBorderRight" onClick={updatePanel}>Setup</button>
-            <button className="noBorderLeft" onClick={updatePanel}>My Servers</button>
-            <br/>
-            {
-                panel === "" || panel === "Setup" ? 
-                (
-                    <Setup />
-                ) : (
-                    <MyServers />
-                )
-            }
+        <div className="appContainer">
+            <div className="dashboardContainer">
+                <Modal message={"Please Login."} />
+                <br/>
+                <h1>Owner Dashboard</h1>
+                <br/>
+                <button className={`noBorderRight ${panel === "Setup" && "selected"}`} onClick={updatePanel}>Setup</button>
+                <button className={`noBorderLeft ${panel === "My Servers" && "selected"}`} onClick={updatePanel}>My Servers</button>
+                <br/>
+                {
+                    panel === "Setup" ? 
+                    (
+                        <Setup />
+                    ) : (
+                        <MyServers />
+                    )
+                }
+            </div> 
         </div>
     );
 }
