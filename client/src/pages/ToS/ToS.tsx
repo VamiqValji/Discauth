8aa9836a502eb0ce26202279648f8bdb09946bf9
Vamiq <vamiq.valji@gmail.com>
2021-04-18 17:23:36 -0400

    minor tos changes

--- client/src/pages/ToS/ToS.tsx
+++ client/src/pages/ToS/ToS.tsx
@@ -18,17 +18,17 @@ const ToS: React.FC<ToSProps> = () => {
             <p>
                 This website is operated by Discauth. Throughout the site, the terms “we”, “us” and “our” refer to Discauth. Discauth offers this website and its Discord bot as a service, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
 
-                By visiting our site and/ or purchasing something from us, or using our Discord bot, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.
+                By visiting our site and/ or purchasing something from us, and or using our Discord bot, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site and Discord bot, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.
 
-                Please read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.
+                Please read these Terms of Service carefully before accessing or using our website and or Discord bot. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.
 
-                Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.
+                Any new features or tools which are added to the current store and or website and service shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.
             
                 All purchases and or payments of any kind are final and non-refundable.
             </p>
 
             <br/>
-            <h4>SECTION 1 - ONLINE STORE TERMS</h4>
+            <h4>SECTION 1 - ONLINE STORE AND SERVICE TERMS</h4>
 
             <p>
                 By agreeing to these Terms of Service, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
@@ -134,7 +134,7 @@ const ToS: React.FC<ToSProps> = () => {
             <h4>SECTION 10 - PERSONAL INFORMATION</h4>
 
             <p>
-                Your submission of personal information through the store is governed by our Privacy Policy. To view our Privacy Policy.
+                Your submission of personal information through the store and service is governed by our Privacy Policy. To view our Privacy Policy.
             </p>
 
             <br/>
